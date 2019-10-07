0fa01d5b5b2a217a77c68cbe77f92bb85e67252f
Venkata Dasu <vdasu@mail.usf.edu>
2019-10-07 14:04:51 -0400

    ok

--- Program.cs
+++ Program.cs
@@ -7,41 +7,12 @@ namespace binary_matrix_image_flip
         static void Main(string[] args)
         {
             try
-            {
-                int[,] A = new int[,] { { 1, 1, 0, 0 }, { 1, 0, 0, 1 }, { 0, 1, 1, 1 }, { 1, 0, 1, 0 } };
-                Console.WriteLine("Length: " + A.Length);
-                Console.WriteLine("Rank: " + A.Rank);
-
-
-                int rows = A.GetLength(0);
-                int columns = A.GetLength(1);
-                Console.WriteLine("Rows: " + rows);
-                Console.WriteLine("Columns: " + columns);
-
-                int[,] B = new int[rows, columns];
-
-
-                for (int i = 0; i < rows; i++)
-                {
-                    columns = A.GetLength(1);
-                    for (int j = 0; j < columns; j++)
-                    {
-                        columns--;
-
-                        B[i, j] = 1 - (A[i, columns]);
-
-
-                        B[i, columns] = 1 - (A[i, j]);
-
-
-                    }
-                }
-
-                foreach (int item in B)
-                {
-                    Console.WriteLine(item + " ");
-                }
-
+            {                
+                int[,] matrix = new int[,] { { 1, 1, 0, 0 }, { 1, 0, 0, 1 }, { 0, 1, 1, 1 }, { 1, 0, 1, 0 } };                
+                int[,] flipAndInvertedImage = FlipAndInvertImage(matrix);
+                Console.WriteLine("The resulting flipped and inverted image is:\n");
+                Display2DArray(flipAndInvertedImage);
+                Console.Write("\n");
 
             }   // End of try
 
@@ -51,6 +22,57 @@ namespace binary_matrix_image_flip
 
             }   // End of catch
 
+        }
+
+        /*
+            This method prints the values of an 2D integer array. 
+        */
+        public static void Display2DArray(int[,] a)
+        {
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    Console.Write(a[i, j] + "\t");
+                }
+                Console.Write("\n");
+            }
+        }
+        // End of Display2DArray
+
+        public static int[,] FlipAndInvertImage(int[,] matrix)
+        {
+            try
+            {
+                int rows = matrix.GetLength(0);
+                int columns = matrix.GetLength(1);
+                
+                int[,] flipAndInvertedImage = new int[rows, columns];
+
+                for (int i = 0; i < rows; i++)
+                {
+                    columns = matrix.GetLength(1);
+                    for (int j = 0; j < columns; j++)
+                    {
+                        columns--;
+
+                        flipAndInvertedImage[i, j] = 1 - (matrix[i, columns]);
+
+                        flipAndInvertedImage[i, columns] = 1 - (matrix[i, j]);
+
+                    }
+                }                         
+
+                return flipAndInvertedImage;
+            }
+
+            catch
+            {
+                Console.WriteLine("Exception occured while computing FlipAndInvertImage()");
+            }
+
+            return new int[,] { };
+            
         }
     }
 }
